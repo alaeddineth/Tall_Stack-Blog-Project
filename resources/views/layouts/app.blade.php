a0e1579f377e1969872814829adc3e6484fe01e2
alaeddineth <theljanialaeddine@gmail.com>
2024-05-14 15:05:22 +0100

    "Home Page | Build Blog with Laravel, Livewire & Filament"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,27 +19,19 @@
     </head>
     <body class="font-sans antialiased">
         <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
+    
+        @include('layouts.partials.header')
+    
+        @yield('hero')
+    
+        <main class="container mx-auto px-5 flex flex-grow">
+            {{ $slot }}
+        </main>
+    
+        @include('layouts.partials.footer')
+    
         @stack('modals')
-
         @livewireScripts
     </body>
+    
 </html>
